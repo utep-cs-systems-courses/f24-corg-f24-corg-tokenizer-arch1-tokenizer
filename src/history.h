36fa542ed58239599c10d1cbe7c5f0b42c4289cb
Zaphryx1 <lalo.gaytan@yahoo.com>
2024-10-14 00:55:48 -0600

    changed histories

--- src/history.h
+++ src/history.h
@@ -1,69 +1,42 @@
-#ifndef HISTORY_H
+#ifndef _HISTORY_H_
 
-#define HISTORY_H
-
-
-
-// Structure for an individual history item in the linked list
-
-typedef struct HistoryItem {
-
-  int id;                // Unique ID of the history item
-
-  char *str;             // String associated with the history item
-
-  struct HistoryItem *next; // Pointer to the next item in the list
-
-} HistoryItem;
+#define _HISTORY_H_
 
 
-
-// Structure representing the entire history list
 
 typedef struct HistoryList {
 
-  HistoryItem *head;     // Pointer to the first item in the history list
+  char* phrase;
+
+  struct HistoryList* next;
 
 } HistoryList;
 
 
 
-/* Initialize a new empty history list.
-
-   Returns a pointer to the newly created history list. */
+// Initialize the history list
 
 HistoryList* init_history();
 
 
 
-/* Add a new history item to the end of the list.
+// Add a phrase to history
 
-   Parameters:
-
-   - list: Pointer to the history list where the item will be added
-
-   - str: The string to be stored in the new history item */
-
-void add_history(HistoryList *list, char *str);
+void add_history(HistoryList* list, char* str);
 
 
-void add_history(HistoryList *list, char *str);
 
-/* Retrieve the string stored in the history item with a matching ID.
-   Parameters:
-   - list: Pointer to the history list
-   - id: The ID of the history item to be retrieved
-   Returns a pointer to the string if found, or NULL if the ID does not exist. */
-char *get_history(HistoryList *list, int id);
+// Print the history list
 
-/* Print all the history items in the list.
-   Parameters:
-   - list: Pointer to the history list */
-void print_history(HistoryList *list);
+void print_history(HistoryList* list);
 
-/* Free all memory allocated for the history list and its items.
-   Parameters:
-   - list: Pointer to the history list to be freed */
-void free_history(HistoryList *list);
 
-#endif // HISTORY_H
+
+// Free the memory used by the history list
+
+void free_history(HistoryList* list);
+
+
+
+#endif
+
